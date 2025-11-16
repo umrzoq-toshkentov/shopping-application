9aa90ab16f53235ba084cb6721a300caf6ab54de
umrzoq-toshkentov <ssdumrzoq@gmail.com>
2025-11-16 18:24:49 +0500

    feat: added validation pipes to check validation

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,31 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsMongoId,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateReviewDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @Max(5)
+  @Min(1)
+  @IsNumber()
   rating: number;
+
+  @IsNotEmpty()
+  @IsMongoId()
   productId: string;
 }
