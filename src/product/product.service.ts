fe7047cd3da35a69d2f96a879a113371f18b933e
umrzoq-toshkentov <ssdumrzoq@gmail.com>
2025-11-17 17:59:17 +0500

    feat: Added Product controller and service

---
+++ src/product/product.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { ProductModel } from './product.model';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { CreateProductDto } from './dto/create-product.dto';
+import { FindProductDto } from './dto/find-product.dto';
+import { ReviewModel } from 'src/review/review.model';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectModel(ProductModel)
+    private readonly productModel: ReturnModelType<typeof ProductModel>,
+  ) {}
+
+  async create(product: CreateProductDto): Promise<ProductModel> {
+    return await this.productModel.create(product);
+  }
+
+  async findById(id: string): Promise<ProductModel> {
+    return await this.productModel.findById(id).exec();
+  }
+
+  async delete(id: string): Promise<ProductModel> {
+    return await this.productModel.findByIdAndDelete(id).exec();
+  }
+
+  async updateById(id: string, product: ProductModel): Promise<ProductModel> {
+    return await this.productModel
+      .findByIdAndUpdate(id, product, { new: true })
+      .exec();
+  }
+
+  async findWithReviews(dto: FindProductDto): Promise<
+    (ProductModel & {
+      review: ReviewModel[];
+      reviewCount: number;
+      reviewAvg: number;
+    })[]
+  > {
+    return (await this.productModel
+      .aggregate([
+        {
+          $match: {
+            categories: dto.category,
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+        {
+          $limit: dto.limit,
+        },
+        {
+          $lookup: {
+            from: 'Review',
+            localField: '_id',
+            foreignField: 'productId',
+            as: 'reviews',
+          },
+        },
+        {
+          $addFields: {
+            reviewCount: { $size: '$reviews' },
+            reviewAvg: { $avg: '$reviews.rating' },
+          },
+        },
+      ])
+      .exec()) as (ProductModel & {
+      review: ReviewModel[];
+      reviewCount: number;
+      reviewAvg: number;
+    })[];
+  }
+}
